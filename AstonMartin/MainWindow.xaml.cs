607fdb4427507c4c4c6a6517d2128bba9fd849aa
edza <edexdev@gmail.com>
2015-01-06 18:38:35 +0200

    humidity, wind speed, pressure

--- AstonMartin/MainWindow.xaml.cs
+++ AstonMartin/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,13 @@ namespace AstonMartin
     {
         const string ADDR = "http://www.worldweatheronline.com/Riga-weather/Riga/LV.aspx";
 
+        readonly Dictionary<char,string> Compass = new Dictionary<char,string>() {
+                {'N', "ziemeļu"},
+                {'S', "dienvidu"},
+                {'W', "rietumu"},
+                {'E', "austrumu"},
+        };
+
         public MainWindow()
         {
             InitializeComponent();
@@ -47,6 +55,7 @@ namespace AstonMartin
         Timer visualTimer = new Timer(100);
 
         string temperature = "ielādēju...";
+        Dictionary<ExtraWeatherInfo, string> extraInfo;
         DateTime lastTempLoaded = DateTime.Now.AddSeconds(-60);
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -60,13 +69,16 @@ namespace AstonMartin
 
         private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            temperature = (string)e.Result;
+            dynamic dyn = e.Result;
+            temperature = dyn.temper;
+            extraInfo = dyn.dict;
         }
 
         private void bw_DoWork(object sender, DoWorkEventArgs e)
         {
-           
-            e.Result = GetTemperature();
+            Dictionary<ExtraWeatherInfo,string> dict;
+            string temper = GetWeather(out dict);
+            e.Result = new { temper, dict };
         }
 
         private void visualTimer_Elapsed(object sender, ElapsedEventArgs e)
@@ -75,10 +87,34 @@ namespace AstonMartin
             this.Dispatcher.Invoke((Action)(() =>
             {
                 tb.Inlines.Clear();
-                tb.Inlines.Add(new Run(DateTime.Now.ToShortDateString() + " "));
-                tb.Inlines.Add(new Run(DateTime.Now.ToShortTimeString()) { Foreground = Brushes.GreenYellow, FontSize = 90 });
+
+                string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Now.Month);
+                string dayName = CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(DateTime.Now.DayOfWeek);
+
+                tb.Inlines.Add(new Run(dayName + ", " + DateTime.Now.Day + ". " + monthName)
+                {
+                    FontSize = 60
+                });
+                tb.Inlines.Add(new Run(DateTime.Now.ToShortTimeString())
+                {
+                    Foreground = Brushes.GreenYellow,
+                    FontSize = 90
+                });
                 tb.Inlines.Add(new Run(":" + DateTime.Now.ToString("ss")));
                 tb.Inlines.Add(new Run(Environment.NewLine + "               " + temperature));
+
+                if (extraInfo != null)
+                {
+                    tb.Inlines.Add(new Run(Environment.NewLine +
+                        "               " + extraInfo[ExtraWeatherInfo.WindSpeed] + Environment.NewLine +
+                        "           " + extraInfo[ExtraWeatherInfo.Humidity] + " mitrums" +
+                        " " + extraInfo[ExtraWeatherInfo.Pressure]
+                        )
+                    {
+                        Foreground = Brushes.Gray,
+                        FontSize = 50
+                    });
+                }
 
                 if (DateTime.Now - lastTempLoaded > new TimeSpan(0, 0, 40))
                 {
@@ -92,37 +128,95 @@ namespace AstonMartin
             visualTimer.Start();
         }
 
-        private string GetTemperature()
+        private string GetWeather(out Dictionary<ExtraWeatherInfo, string> extraInfo)
         {
-            
-
-            string temperatureLocal;
+            string temper;
 
             try
             {
-            HtmlAgilityPack.HtmlWeb web = new HtmlWeb();
-            HtmlAgilityPack.HtmlDocument doc =
-                web.Load(ADDR);
+                HtmlAgilityPack.HtmlWeb web = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = web.Load(ADDR);
 
-            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//*[contains(@class, 'outlook_box1')]");
-
-            
-                temperatureLocal = "error loading temperature";
-
-                foreach (HtmlNode row in nodes)
-                {
-                    temperatureLocal = HttpUtility.HtmlDecode(row.ChildNodes[1].InnerText);
-                }
-                
+                temper = ExtractTemperature(doc);
+                extraInfo = ExtractExtraInfo(doc);
             }
             catch
             {
-                temperatureLocal = "neizdevās!";
+                temper = "neizdevās!";
+                extraInfo = null;
             }
 
-            
+            return temper;
+        }
 
-            return temperatureLocal;
+        enum ExtraWeatherInfo { WindSpeed = 0, Humidity = 1, Pressure = 2}
+
+        private Dictionary<ExtraWeatherInfo, string> ExtractExtraInfo(HtmlDocument doc)
+        {
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//*[contains(@class, 'outlook_right')]");
+            
+            string windSpeed = "", humidity = "", pressure = "";
+            int count = 0;
+
+            foreach (HtmlNode row in nodes)
+            {
+                string item = HttpUtility.HtmlDecode(row.InnerText);
+
+                switch (count)
+                {
+                    case (int)ExtraWeatherInfo.WindSpeed:
+                        windSpeed = ParseWindspeed(item);
+                        break;
+                    case (int)ExtraWeatherInfo.Humidity:
+                        humidity = item;
+                        break;
+                    case (int)ExtraWeatherInfo.Pressure:
+                        pressure = item;
+                        break;
+                }
+                count++;
+            }
+
+            return new Dictionary<ExtraWeatherInfo, string>() {
+                    {ExtraWeatherInfo.WindSpeed, windSpeed},
+                    {ExtraWeatherInfo.Humidity, humidity},
+                    {ExtraWeatherInfo.Pressure, pressure},
+                };
+        }
+
+        private string ParseWindspeed(string item)
+        {
+            string windSpeed = item.Trim();
+
+            windSpeed = MphtoMs(windSpeed);
+
+            windSpeed = windSpeed.Replace("mph from the", "m/s");
+            char lastChar = windSpeed[windSpeed.Length - 1];
+            string fixedStr = Compass[lastChar];
+            windSpeed = windSpeed.Replace(lastChar.ToString(), fixedStr);
+
+            return windSpeed;
+        }
+
+        private static string MphtoMs(string windSpeed)
+        {
+            string firstNums = String.Concat(windSpeed.TakeWhile(c => char.IsDigit(c)).ToList<char>());
+            int windSpeedMph = int.Parse(firstNums);
+            double windSpeedMs = Math.Round(0.44704 * windSpeedMph, 1);
+            windSpeed = windSpeed.Remove(0, windSpeedMph.ToString().Length);
+            return windSpeedMs + windSpeed;
+        }      
+
+        private static string ExtractTemperature(HtmlAgilityPack.HtmlDocument doc)
+        {
+            string temper = "gļuks";
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//*[contains(@class, 'outlook_box1')]");
+
+            foreach (HtmlNode row in nodes)
+            {
+                temper = HttpUtility.HtmlDecode(row.ChildNodes[1].InnerText);
+            }
+            return temper;
         }
 
 
